30ad2b63cb10277b2f4b5625c4e8812e187f5573
Ilihon <Ilia.burdukovskiy@gmail.com>
2019-11-27 13:54:15 +0700

    Add files via upload
    Started 3 new classes

---
+++ main.cpp
@@ -0,0 +1,35 @@
+
+
+#ifndef FUC_H
+#define FUC_H
+
+
+
+#define YES true
+#define NO false
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <conio.h>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+
+
+using namespace std;
+
+
+class menu{
+	
+};
+
+class level{
+	
+};
+
+class player{
+	
+};
+
